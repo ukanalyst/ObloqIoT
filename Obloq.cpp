28855de407e0194feb4610b9b79722d89038fa83
thycoding <49593576+thycoding@users.noreply.github.com>
2020-11-28 16:19:33 +0800

    Update Obloq.cpp

--- Obloq.cpp
+++ Obloq.cpp
@@ -4,7 +4,7 @@ namespace Obloq {
 
     //%
     void obloqSetTxBufferSize(int size){
-        if(size > 100) {
+        if(size > 500) {
             size = 500;
         }
         uBit.serial.setTxBufferSize(size);
@@ -12,7 +12,7 @@ namespace Obloq {
 
     //%
     void obloqSetRxBufferSize(int size){
-        if(size > 100) {
+        if(size > 500) {
             size = 500;
         }
         uBit.serial.setRxBufferSize(size);
@@ -46,12 +46,11 @@ namespace Obloq {
       }
     }
 
-     //%
+    //%
     void obloqWriteString(StringData *text) {
       if (!text) {
           return;
       }
       uBit.serial.send(ManagedString(text));
     }
-
 }
